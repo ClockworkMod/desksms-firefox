3d0370400147129a034fd08abeff90ac0a430c23
Koushik Dutta <koushd@gmail.com>
2011-07-19 15:56:06 -0700

    secure

--- dsmsScript.js
+++ dsmsScript.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var url ="http://2.desksms.appspot.com/api/v1/user/DSMS.clockwork@gmail.com/sms";
+    var url ="https://2.desksms.appspot.com/api/v1/user/DSMS.clockwork@gmail.com/sms";
     $.get(url,
     function(data,textStatus,jqXHR)
     {
